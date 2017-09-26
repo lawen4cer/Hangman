5985828b020306dcc21903163bae7ba00d108064
William Rice <william.rice192@gmail.com>
2017-09-26 16:04:04 -0400

    updated game loop to play 1 game
    still need to implement win/loss logic

--- src/com/Personal/WRice/Game.java
+++ src/com/Personal/WRice/Game.java
@@ -1,6 +1,7 @@
 package com.Personal.WRice;
 
 public class Game {
+    public static final int MAX_MISSES = 7;
     private String answer;
     private String hits;
     private String misses;
@@ -19,6 +20,24 @@ public class Game {
             misses += letter;
         }
         return isHit;
+
+    }
+
+    public int getRemainingTries() {
+        return MAX_MISSES - misses.length();
+
+    }
+
+    public String getCurrentProgress() {
+        String progress = "";
+        for (char letter : answer.toCharArray()) {
+            char display = '-';
+            if(hits.indexOf(letter) != -1) {
+                display = letter;
+            }
+            progress += display;
+        }
+        return progress;
 
     }
 }
--- src/com/Personal/WRice/Hangman.java
+++ src/com/Personal/WRice/Hangman.java
@@ -3,6 +3,11 @@ package com.Personal.WRice;
 public class Hangman {
 
     public static void main(String[] args) {
-    Game game = new Game("treehouse");
+        Game game = new Game("treehouse");
+        Prompter prompter = new Prompter(game);
+        while (game.getRemainingTries() > 0){
+            prompter.displayProgress();
+            prompter.promptForGuess();
+        }
     }
 }
