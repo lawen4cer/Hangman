a683ded77bd2285991c2523e58c2b8e26a1d2445
Billy Rice <william.rice192@gmail.com>
2017-09-26 22:15:39 -0400

    normalize guess and a couple exceptions
    added normallize guess method and added exceptions after discovering some bugs

--- src/com/Personal/WRice/Prompter.java
+++ src/com/Personal/WRice/Prompter.java
@@ -10,10 +10,22 @@ class Prompter {
 
     public boolean promptForGuess() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a letter:  ");
-        String guessInput = scanner.nextLine();
-        char guess = guessInput.charAt(0);
-        return game.applyGuess(guess);
+        boolean isHit = false;
+        boolean isAcceptable = false;
+
+             do {
+                     System.out.print("Enter a letter:  ");
+                     String guessInput = scanner.nextLine();
+                     char guess = guessInput.charAt(0);
+
+                        try {
+                            isHit = game.applyGuess(guess);
+                            isAcceptable = true;
+                            } catch (IllegalArgumentException iae) {
+                             System.out.printf("%s.  Please try again. %n", iae.getMessage());
+                        }
+                 } while(! isAcceptable);
+        return isHit;
     }
 
     public void displayProgress() {
